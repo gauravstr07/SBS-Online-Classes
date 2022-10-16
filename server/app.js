868f85ee602f49e819809b5bcfca3d7d4de8b010
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-16 16:17:38 +0530

    Error Handling

--- server/app.js
+++ server/app.js
@@ -6,9 +6,19 @@ const app = express();
 app.use(express.json());
 const port = 5000;
 
+
+// Routes
 app.use("/student", studentRoute);
 app.use("/faculty", facultyRoute);
 
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        error: 'Page not found'
+    })
+})
+
+// Calling to Server
 app.use((req, res, next) => {
   res.status(200).json({
     message: `Server running on port: ${port}📡`,
