d6e52d6d1bb23fd322f719a4623f65d3970252ce
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-10-16 16:38:41 +0530

    Connetion to DB

--- server/app.js
+++ server/app.js
@@ -1,22 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const studentRoute = require("./api/routes/student");
 const facultyRoute = require("./api/routes/faculty");
+
+mongoose
+  .connect("mongodb://localhost:27017/sbs-rest-api", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connection successfull to mongoDB😍");
+  })
+  .catch((err) => {
+    console.log(`Something went wrong in DB 😪 --------- ${err}`);
+  });
 
 const app = express();
 app.use(express.json());
 const port = 5000;
 
-
 // Routes
 app.use("/student", studentRoute);
 app.use("/faculty", facultyRoute);
 
-
 app.use((req, res, next) => {
-    res.status(404).json({
-        error: 'Page not found'
-    })
-})
+  res.status(404).json({
+    error: "Page not found",
+  });
+});
 
 // Calling to Server
 app.use((req, res, next) => {
